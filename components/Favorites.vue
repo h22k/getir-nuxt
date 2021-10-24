4aaf854ea1040088783248b8f4833b3e50118ead
Halil Hakan Karabay <hakankarabay@icloud.com>
2021-10-24 08:06:10 +0300

    code refactored

--- components/Favorites.vue
+++ components/Favorites.vue
@@ -2,21 +2,7 @@
 <div class="mt-8 max-w-screen-lg min-w-[1200px] mx-auto">
   <Title title="Favoriler"/>
   <div class="grid bg-white grid-cols-8 divide-gray-50 divide-y-2 divide-x-2">
-    <div v-for="product in products" :key="product.id" class="flex flex-col gap-y-2 w-full p-3 !pb-0 items-center">
-      <div class="relative w-[120px] h-[120px]">
-        <img :src="product.image" class="w-full object-contain" alt="">
-        <span class="absolute cursor-pointer inline-flex bg-white justify-center items-center text-getir/70 border-2 p-2 rounded-lg shadow-sm w-8 h-8 top-0 right-0">
-          <client-only>
-            <v-icon name="plus" scale=".7"/>
-          </client-only>
-        </span>
-      </div>
-      <div class="flex justify-center items-center">
-        <span class="text-getir font-semibold">{{ product.price | currency }}</span>
-      </div>
-      <span class="text-black text-center text-sm leading-4 font-semibold">{{ product.title }}</span>
-      <span class="text-black/40 text-center">{{ product.alt }}</span>
-    </div>
+    <Product v-for="product in products" :key="product.id" :product="product"/>
   </div>
 </div>
 </template>
@@ -25,9 +11,10 @@
 import Title from "./widgets/public/Title";
 
 import products from '../static/api/products.json'
+import Product from "./widgets/Product/Product";
 export default {
   name: "Favorites",
-  components: {Title},
+  components: {Product, Title},
   data() {
     return {
       products: []
---
+++ components/widgets/Product/Product.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex flex-col gap-y-2 w-full p-3 !pb-0 items-center">
+    <div class="relative w-[120px] h-[120px]">
+      <img :src="product.image" class="w-full object-contain" alt="">
+      <span class="absolute cursor-pointer inline-flex bg-white justify-center items-center text-getir/70 border-2 p-2 rounded-lg shadow-sm w-8 h-8 top-0 right-0">
+          <client-only>
+            <v-icon name="plus" scale=".7"/>
+          </client-only>
+        </span>
+    </div>
+    <div class="flex justify-center items-center">
+      <span class="text-getir font-semibold">{{ product.price | currency }}</span>
+    </div>
+    <span class="text-black text-center text-sm leading-4 font-semibold">{{ product.title }}</span>
+    <span class="text-black/40 text-center">{{ product.alt }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Product",
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
